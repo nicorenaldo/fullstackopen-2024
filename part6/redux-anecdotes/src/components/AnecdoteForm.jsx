c2f8e7d72f80d950203e55a22d3c476dda229153
nicorenaldo <nico.renaldo@tuni.fi>
2025-01-12 10:48:15 +0200

    part6: section a

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { createAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    if (content.length > 0) {
+      dispatch(createAnecdote(content));
+      event.target.anecdote.value = '';
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input name='anecdote' />
+      <button>create</button>
+    </form>
+  );
+};
+
+export default AnecdoteForm;
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { vote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const onVote = (id) => {
+    dispatch(vote(id));
+  };
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => onVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
